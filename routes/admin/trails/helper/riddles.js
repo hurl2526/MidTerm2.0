2ed3830faabf6ab74e73176eb5701995d980d0e9
Patrick <patrick.hurley@codeimmersives.com>
2020-07-27 22:01:55 -0400

    some more styling done

--- routes/admin/trails/helper/riddles.js
+++ routes/admin/trails/helper/riddles.js
@@ -1,20 +1,20 @@
 let riddles = [
-  // { id: 1, question: 'Until I am measured I am not known.</br>Yet how you miss me when I have flown.', answer: 'time' },
-  // { id: 2, question: 'When people board this type of transport</br>Into different cabins they are split</br>It has wings, a nose and a tail</br>And the pilots fly in a cockpit2', answer: 'airplane' },
-  // { id: 3, question: 'If you drop me Im sure to crack,</br>but give me a smile and Ill always smile back.', answer: 'mirror' },
+  { id: 1, question: 'Until I am measured I am not known.Yet how you miss me when I have flown.', answer: 'time' },
+  { id: 2, question: 'When people board this type of transport. Into different cabins they are split. It has wings, a nose and a tail And the pilots fly in a cockpit.', answer: 'airplane' },
+  { id: 3, question: 'If you drop me Im sure to crack,but give me a smile and Ill always smile back.', answer: 'mirror' },
   { id: 4, question: 'What always runs but never walks,often murmurs, never talks, has a bed but never sleeps, has a mouth but never eats?', answer: 'river' },
-  { id: 5, question: `I am the beginning of the end, and the end of time and space.%0D
+  { id: 5, question: `I am the beginning of the end, and the end of time and space.
   I am essential to creation, 
   and I surround every place.`, answer: 'e' },
-  { id: 6, question:`The one who makes it always sells it.%0D
+  { id: 6, question:`The one who makes it always sells it.
   The one who buys it never uses it.
   The one who uses it never knows hes using it.`, answer: 'coffin' },
-  // { id: 7, question: 'riddle3', answer: 'answer3' },
-  // { id: 8, question: 'riddle3', answer: 'answer3' },
-  // { id: 9, question: 'riddle3', answer: 'answer3' },
-  // { id: 10, question: 'riddle3', answer: 'answer3' },
-  // { id: 11, question: 'riddle3', answer: 'answer3' },
-  // { id: 12, question: 'riddle3', answer: 'answer3' },
+  { id: 7, question: 'Whats black when you get it, red when you use it, and white when youre done with it?', answer: 'charcoal' },
+  { id: 8, question: 'Each morning I appear to lie at your feet. All day I will follow no matter how fast you run, yet I nearly perish in the midday sun.', answer: 'shadow' },
+  { id: 9, question: 'At the sound of me, men may dream or stamp their feet.At the sound of me, women may laugh or sometimes weep', answer: 'music' },
+  { id: 10, question: 'Youve heard me before, yet you hear me again,Then I die til you call me again.', answer: 'echo' },
+  { id: 11, question: 'What does man love more than life, Fear more than death or mortal strife. What the poor have, the rich require, and what contented men desire. What the miser spends and the spendthrift saves, And all men carry to their graves?', answer: 'nothing' },
+  { id: 12, question: 'Reaching stiffly for the sky, I bare my fingers when its cold. In warmth I wear an emerald glove, and in between I dress in gold.', answer: 'tree' },
   // { id: 13, question: 'riddle3', answer: 'answer3' },
   // { id: 14, question: 'riddle3', answer: 'answer3' },
   // { id: 15, question: 'riddle3', answer: 'answer3' },
